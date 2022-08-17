768cea48b5191c54aa1b869b67230f957e5ce970
prabath1998 <prabhathudayanga1998@gmail.com>
2022-08-17 19:03:37 +0530

    java bean configuration

--- SpringFrameworkDemo/src/main/java/demo/Doctor.java
+++ SpringFrameworkDemo/src/main/java/demo/Doctor.java
@@ -2,14 +2,13 @@ package demo;
 
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class Doctor implements Staff {
 
 
     public void assist() {
         System.out.println("Doctor is assisting");
     }
-
 
 
 }
--- SpringFrameworkDemo/src/main/java/demo/Main.java
+++ SpringFrameworkDemo/src/main/java/demo/Main.java
@@ -1,12 +1,15 @@
 package demo;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
 
         Staff staff = context.getBean(Nurse.class);
         staff.assist();
