2528145aeaac6e4f9334dc06891f36deb4c7d908
prabath1998 <prabhathudayanga1998@gmail.com>
2022-08-18 14:23:19 +0530

    Add crud operations,Exception handling,loggers,validations

---
+++ springboot-master/src/test/java/com/test/springbootmaster/SpringbootMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.springbootmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
