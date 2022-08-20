41b14dfb3548c7d094cf286c9db7d103c999b751
prabath1998 <prabhathudayanga1998@gmail.com>
2022-08-20 23:33:01 +0530

    relationships

---
+++ springdata-jpa/src/main/java/com/test/springdatajpa/entity/Course.java
@@ -0,0 +1,29 @@
+package com.test.springdatajpa.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Course {
+    @Id
+    @SequenceGenerator(
+            name = "course_sequence",
+            sequenceName = "course_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "course_sequence"
+    )
+    private Long courseId;
+    private String title;
+    private Integer credit;
+}
