971306aa005da136fbbbc3f3b236d0473a66295c
prabath1998 <prabhathudayanga1998@gmail.com>
2022-08-20 13:33:49 +0530

    add some query methods

---
+++ springdata-jpa/src/main/java/com/test/springdatajpa/repository/StudentRepository.java
@@ -0,0 +1,26 @@
+package com.test.springdatajpa.repository;
+
+import com.test.springdatajpa.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Long> {
+
+    public List<Student> findByFirstName(String firstName);
+
+    public List<Student> findByFirstNameContaining(String name);
+
+    public  List<Student> findByLastNameNotNull();
+
+    public  List<Student> findByGuardianName(String guardianName);
+
+    Student findByFirstNameAndLastName(String firstName,String lastName);
+
+    //JPQL
+    @Query("select s from Student s where s.emailId = ?1")
+    Student getStudentByEmailAddress(String emailId);
+}
---
+++ springdata-jpa/src/test/java/com/test/springdatajpa/repository/StudentRepositoryTest.java
@@ -0,0 +1,82 @@
+package com.test.springdatajpa.repository;
+
+import com.test.springdatajpa.entity.Guardian;
+import com.test.springdatajpa.entity.Student;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StudentRepositoryTest {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Test
+    public void saveStudent(){
+        Student student = Student.builder()
+                .emailId("prabath@gmail.com")
+                .firstName("prabath")
+                .lastName("udayanga")
+//                .guardianName("priyantha")
+//                .guardianEmail("priya@gmail.com")
+//                .guardianMobile("0778565223")
+                .build();
+        studentRepository.save(student);
+    }
+
+    @Test
+    public void saveStudentWithGuardian(){
+
+        Guardian guardian = Guardian.builder()
+                .name("samantha")
+                .email("sama@gmail.com")
+                .mobile("0223654784")
+                .build();
+
+        Student student = Student.builder()
+                .firstName("janaka")
+                .lastName("chathuranga")
+                .emailId("jana@gmail.com")
+                .guardian(guardian)
+                .build();
+
+        studentRepository.save(student);
+    }
+
+    @Test
+    public void getAllStudents(){
+        List<Student> studentList = studentRepository.findAll();
+        System.out.println(studentList);
+    }
+
+    @Test
+    public void getStudentByFirstName(){
+        List<Student> students = studentRepository.findByFirstName("janaka");
+        System.out.println(students);
+    }
+
+    @Test
+    public void getStudentByFirstNameContaining(){
+        List<Student> students = studentRepository.findByFirstNameContaining("p");
+        System.out.println(students);
+    }
+
+    @Test
+    public void getStudentBasedOnGuardianName(){
+        List<Student> students = studentRepository.findByGuardianName("priyantha");
+        System.out.println(students);
+    }
+
+    @Test
+    public void printStudentByEmailAddress(){
+        Student student = studentRepository.getStudentByEmailAddress("prabath@gmail.com");
+        System.out.println(student);
+    }
+
+}
